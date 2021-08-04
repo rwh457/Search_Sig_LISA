5ff852162e9c7a135f1317cbb7bb6ac8e0aa628f
RWH <321196671@qq.com>
2021-08-04 16:53:00 +0800

    change the length of noise

--- data_gen/noise_gen.py
+++ data_gen/noise_gen.py
@@ -4,22 +4,21 @@ import pycbc.types
 from tqdm import tqdm
 
 
-psd = np.load('psd_4096.npy')
-Tobs = 61425.0
-delta_t = 15.0
-df = 1 / Tobs
-tsamples = int(Tobs / delta_t) + 1
-psd = pycbc.types.frequencyseries.FrequencySeries(psd, delta_f=df)
-
-
 def noise_gen(N_s, dt, psd_v):
     noise = pycbc.noise.noise_from_psd(N_s, dt, psd_v)
     return noise
 
 
-N = 4500
-noise_matrix = np.zeros((N, tsamples))
-for i in tqdm(range(N)):
-    noise_matrix[i, :] = noise_gen(tsamples, delta_t, psd)
+if __name__ == '__main__':
 
-np.save('noise_matrix', noise_matrix)
+    psd = np.load('psd_16384_15s.npy')
+    Tobs = 16384 * 15.0
+    delta_t = 15.0
+    df = 1 / Tobs
+    tsamples = int(Tobs / delta_t) + 1
+    psd = pycbc.types.frequencyseries.FrequencySeries(psd, delta_f=df)
+
+    N = 4500
+    noise_matrix = np.zeros((N, tsamples))
+    for i in tqdm(range(N)):
+        noise_matrix[i, :] = noise_gen(tsamples, delta_t, psd)
