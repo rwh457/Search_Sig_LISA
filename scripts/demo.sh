b9126c60e5f038e6aef21e948e5f09b58e483bae
iphysresearch <hewang@mail.bnu.edu.cn>
2021-09-02 20:31:16 +0800

    Init demo script

---
+++ scripts/demo.sh
@@ -0,0 +1,33 @@
+export CUDA_VISIBLE_DEVICES=0
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+
+python ../model/main.py train new \
+    --data_dir ../data \
+    --model_dir ../output/demo_model \
+    --save_model_name model.pt \
+    --data.z 1 \
+    --data.epoch_size 6 \
+    --data.batch_size 2 \
+    --data.num_workers 0 \
+    --train.num_epochs 3 \
+    --train.lr 0.00001 \
+    --train.no_lr_annealing \
+    --train.lr_anneal_method cosine \
+    --train.output_freq 1 \
+
+# python ../model/main.py train existing \
+#     --data_dir ../data \
+#     --model_dir ../output/demo_model \
+#     --save_model_name model.pt \
+#     --data.z 2 \
+#     --data.epoch_size 64 \
+#     --data.batch_size 2 \
+#     --data.num_workers 0 \
+#     --train.num_epochs 5 \
+#     --train.lr 0.0001 \
+#     --train.no_lr_annealing \    
+#     --train.lr_anneal_method cosine \
+#     --train.output_freq 5 \
