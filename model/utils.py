871d83110e42b62003fda72a37872015cefffac5
iphysresearch <hewang@mail.bnu.edu.cn>
2021-09-02 17:18:53 +0800

    Add writer_row func

--- model/utils.py
+++ model/utils.py
@@ -1,4 +1,12 @@
+import csv
+
 
 def print_dict(d: dict,  ncol: int, prex: str = ''):
     temp = [f"{k}: {v}" for k, v in d.items()]
     print(prex+('\n'+prex).join([', '.join(temp[i:: ncol]) for i in range(ncol)]))
+
+
+def writer_row(p, filename, mode, contentrow):
+    with open(p / filename, mode) as f:
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow(contentrow)
