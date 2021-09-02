86213e5ca152f58370c1ab2709b1c129e44d59d5
iphysresearch <hewang@mail.bnu.edu.cn>
2021-09-02 17:22:51 +0800

    Add ffname func

--- model/utils.py
+++ model/utils.py
@@ -1,3 +1,5 @@
+import os
+import fnmatch
 import csv
 
 
@@ -10,3 +12,23 @@ def writer_row(p, filename, mode, contentrow):
     with open(p / filename, mode) as f:
         writer = csv.writer(f, delimiter='\t')
         writer.writerow(contentrow)
+
+
+def ffname(path, pat):
+    """Return a list containing the names of the files in the directory matches PATTERN.
+
+    'path' can be specified as either str, bytes, or a path-like object.  If path is bytes,
+        the filenames returned will also be bytes; in all other circumstances
+        the filenames returned will be str.
+    If path is None, uses the path='.'.
+    'Patterns' are Unix shell style:
+        *       matches everything
+        ?       matches any single character
+        [seq]   matches any character in seq
+        [!seq]  matches any char not in seq
+    Ref: https://stackoverflow.com/questions/33743816/how-to-find-a-
+        filename-that-contains-a-given-string
+    """
+    return [filename
+            for filename in os.listdir(path)
+            if fnmatch.fnmatch(filename, pat)]
